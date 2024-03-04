e2001282ef575f1745726d4f0bb180dc2493b397
ekirchst <ekirchst@uci.edu>
2024-03-04 15:44:41 -0800

    second commit basic api retrieval done

---
+++ code/WorkoutProject4.py
@@ -0,0 +1,41 @@
+import json as js
+from urllib import request, error
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+class weatherAPI:
+    def __init__(self, key):
+        self.url = "https://api.weatherbit.io/v2.0"
+        self.api_key = key
+
+
+    def get_curr_weather(self, city):
+        url = f"{self.url}/current"
+        params = {
+            "key": self.api_key,
+            "city": city
+        }
+        full = f"{url}?{'&'.join([f'{k}={v}' for k, v in params.items()])}"
+        response = request.urlopen(full)
+        re = js.loads(response.read())
+        print(re)
+        return re
+
+def write_to_json(data, filename):
+    with open(filename, "w") as file:
+        js.dump(data, file)
+
+
+def start():
+    key = "53d3ac0dbfab4f74bf59d721b484fd05"
+    curr_weather = weatherAPI(key)
+    city = "Oakland"
+    curr_weather_data = curr_weather.get_curr_weather(city)
+    write_to_json(curr_weather_data, "current weather.json")
+
+
+
+
+
+if __name__ == "__main__":
+    start()
